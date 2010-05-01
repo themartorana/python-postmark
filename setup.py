ffd6e8a2ddaa13807d073a829cf68b5f7ecba1d6
Maximillian Dornseif <md@hudora.de>
2010-05-02 00:37:20 +0200

    support setup.py and standard Python packaging
    See http://guide.python-distribute.org/ for the general concept.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='python-postmark',
+    version='0.2.0',
+    url='http://github.com/themartorana/python-postmark/',
+    author='Dave Martorana',
+    author_email='?',
+    packages=['postmark',],
+)
