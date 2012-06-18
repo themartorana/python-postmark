510ff9d5e59c105671712aa30576da6e822aee7c
Dave Martorana <themartorana@yahoo.com>
2012-06-18 17:06:41 -0400

    Version bump, preparing for pip update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = open(readme).read()
 
 setup(
     name = "python-postmark",
-    version = "0.3.1",
+    version = "0.3.2",
     packages = find_packages(),
     
     author = "Dave Martorana (http://davemartorana.com), Richard Cooper (http://frozenskys.com), Bill Jones (oraclebill), Dmitry Golomidov (deeGraYve)",
